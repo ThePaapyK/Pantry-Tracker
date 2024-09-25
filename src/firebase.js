f616dd95d7084efc085da75e336ae6de04a0bd3e
Paa Kojo Annan <paaqwadwo419@gmail.com>
2024-09-25 21:08:30 +0000

    microsoft auth issue resolved

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,3 +26,4 @@ const auth = getAuth(app);
 export { app, firestore, auth };
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
+export const microsoftProvider = new OAuthProvider('microsoft.com');
